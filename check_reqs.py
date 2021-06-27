4e45f3f219ce45ceb3162cb09a4830c430589af7
IvanB <corps.des.nombres@gmail.com>
2021-06-27 12:41:31 -0700

    remove apex from main_supcon

---
+++ check_reqs.py
@@ -0,0 +1,14 @@
+import tensorboard_logger as tb_logger
+import torch
+import torch.backends.cudnn as cudnn
+from torchvision import transforms, datasets
+
+from util import TwoCropTransform, AverageMeter
+from util import adjust_learning_rate, warmup_learning_rate
+from util import set_optimizer, save_model
+from networks.resnet_big import SupConResNet
+from losses import SupConLoss
+
+print('passed imports')
+print('check if cuda is available')
+print(torch.cuda.is_available())
