5a4c9ec541a16e7c6736fed5e9bf9cea985f2690
Ivan Barrientos <corps.des.nombres@gmail.com>
2021-07-18 18:44:21 -0700

    add fashion to dataset arg "choices"

--- main_supcon.py
+++ main_supcon.py
@@ -49,7 +49,7 @@ def parse_option():
     # model dataset
     parser.add_argument('--model', type=str, default='resnet18')
     parser.add_argument('--dataset', type=str, default='cifar10',
-                        choices=['cifar10', 'cifar100', 'path'], help='dataset')
+                        choices=['cifar10', 'cifar100', 'fashion', 'path'], help='dataset')
     parser.add_argument('--mean', type=str, help='mean of dataset in path in form of str tuple')
     parser.add_argument('--std', type=str, help='std of dataset in path in form of str tuple')
     parser.add_argument('--data_folder', type=str, default=None, help='path to custom dataset')
